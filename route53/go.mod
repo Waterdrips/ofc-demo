ff8468d924cc076f9fa54e0e4f64cde716083663
Alistair Hey <alistair@heyal.co.uk>
2020-12-17 19:24:23 +0000

    Add secrets
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

---
+++ route53/go.mod
@@ -0,0 +1,8 @@
+module github.com/Waterdrips/ofc-demo/route53
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.36.10
+	github.com/openfaas/openfaas-cloud/sdk v0.0.0-20201215121043-ffbb84724081
+)
---
+++ route53/handler.go
@@ -0,0 +1,53 @@
+package function
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/route53"
+	"net/http"
+	"os"
+
+	"github.com/openfaas/openfaas-cloud/sdk"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+
+	readSecrets()
+	sess := session.Must(session.NewSession())
+	client := route53.New(sess)
+	z,err := client.ListHostedZones(&route53.ListHostedZonesInput{})
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	var records []*route53.ResourceRecordSet
+	for _, zone := range z.HostedZones {
+
+		r, err :=  client.ListResourceRecordSets(&route53.ListResourceRecordSetsInput{HostedZoneId: zone.Id})
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusOK)
+		}
+		for _, s := range r.ResourceRecordSets {
+			records = append(records, s)
+		}
+	}
+
+	s := ""
+
+	for _, r := range records {
+		s = fmt.Sprintf("%s\n%s", s, *r.Name)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func readSecrets() {
+	id, _ := sdk.ReadSecret("access-key-id")
+	key, _ := sdk.ReadSecret("secret-access-key")
+
+	os.Setenv("AWS_ACCESS_KEY_ID", id)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", key)
+
+}
