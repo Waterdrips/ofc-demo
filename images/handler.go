4b6946f816e6d52ced4739880312dd3f887d9faf
Alistair Hey <alistair@heyal.co.uk>
2020-12-18 14:01:15 +0000

    fix uploading file
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- images/handler.go
+++ images/handler.go
@@ -1,6 +1,7 @@
 package function
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -28,12 +29,29 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
 
+		// Parse our multipart form, 10 << 20 specifies a maximum
+		// upload of 10 MB files.
+		r.ParseMultipartForm(10 << 20)
+		// FormFile returns the first file for the given key `myFile`
+		// it also returns the FileHeader so we can get the Filename,
+		// the Header and the size of the file
+		file, handler, err := r.FormFile("image")
+		if err != nil {
+			fmt.Println("Error Retrieving the File")
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+		fmt.Printf("File Size: %+v\n", handler.Size)
+		fmt.Printf("MIME Header: %+v\n", handler.Header)
+
 		minioClient.MakeBucket(bucketName, region)
 
 		defer r.Body.Close()
-		_, err := minioClient.PutObject(bucketName,
+		_, err = minioClient.PutObject(bucketName,
 			imgPath,
-			r.Body,
+			file,
 			r.ContentLength,
 			minio.PutObjectOptions{})
 
